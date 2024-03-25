8f4ef6884b96a372879060d09d6c5166038a619f
stanleyedward <stanleyedward9715@gmail.com>
2024-03-26 00:21:41 +0530

    rev change

--- 3D_reconstruction_modules/rendering.py
+++ 3D_reconstruction_modules/rendering.py
@@ -22,7 +22,7 @@ def rendering(model, rays_origin, rays_direction, tn, tf, nb_bins = 100, device 
     #query the color and the denisty at each point X to
     # comput the integral
     
-    colors, density = model.intersect(x.reshape(-1, 3), rays_direction.expand(x.shape[1], x.shape[0], 3).transpose(0, 1).reshape(-1, 3))
+    colors, density = model.intersect(x.reshape(-1, 3))
     
     colors = colors.reshape((x.shape[0], nb_bins, 3)) #shape [nb_rays, nb_bins, 3]
     density = density.reshape((x.shape[0], nb_bins, 1)) #[nb_rays, nb_bins, 1]
